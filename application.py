0ccb57fb9f3a8b7b2bf743e21e50443f0fadd117
Rrikochet <lionelyong.jh@gmail.com>
2022-09-21 04:50:41 +0800

    Working household_create and DB

--- application.py
+++ application.py
@@ -1,12 +1,28 @@
-from flask import Flask,redirect, url_for,render_template,send_from_directory, request, session
+from flask import Flask,redirect, url_for,render_template, url_for, flash, send_from_directory, request, session
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 import secrets
 import string
+
 
 app = Flask(__name__)
 key = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(32))
 app.secret_key = key
 app.permanent_session_lifetime = timedelta(hours=1)
+
+# Create Empty Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+db.create_all()
+
+# Classes
+class Household(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hType = db.Column(db.String(20), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return f"Household('{self.id}', '{self.hType}')"
+
 
 @app.route("/")
 def home():
@@ -25,12 +41,19 @@ def pagenotfound():
 def admin():
     return redirect(url_for("pagenotfound"))
     
-@upload.route("/create",methods=["POST"])
-def display_chart():
-    hType = request.form["hType"]
-    # forward_message = algorithm.init(hType)
-    # target = user_input
-    return render_template('household_create.html', forward_message=forward_message,target=target);
+@app.route("/create")
+def display_household_create():
+    return render_template('household_create.html');
+    
+@app.route("/create",methods=["POST"])
+def submit_household_create():
+    db.create_all()
+    hT = request.form["hType"]
+    db.session.add(Household(hType=hT))
+    db.session.commit()
+    last = str(db.session.query(Household).order_by(Household.id.desc()).first())
+    createMessage = last + ' has been created. '
+    return render_template('household_create.html', createMessage=createMessage);
 
 if __name__ == "__main__":
     app.run()
