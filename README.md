d21e5a48556e8b6fe0c7e90b1a8bd6ec8a0a423b
Rrikochet <lionelyong.jh@gmail.com>
2022-09-21 15:17:29 +0800

    Runable household_add_member

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # TAP-Meteor-2023
 TAP 2023 Div Tech Assessment - Meteor
+
+Assumptions made for the assessment in general:
+1. 
+	Authentication is not required.
+
+
+Assumptions made for End-Points specified:
+1. a. 
+	It is assumed that "create" the household is done automatically, 
+	an id (primary key) is given to the newly created household to identify it.
+1. b.
+	The only Housing Types are mentioned. Landed, Condominium, HDB. 
--- application.py
+++ application.py
@@ -1,16 +1,18 @@
+# Imports
 from flask import Flask,redirect, url_for,render_template, url_for, flash, send_from_directory, request, session
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 import secrets
 import string
 
-
+# Important
 app = Flask(__name__)
 key = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(32))
 app.secret_key = key
 app.permanent_session_lifetime = timedelta(hours=1)
 
-# Create Empty Database
+# Create Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 db.create_all()
@@ -22,8 +24,22 @@ class Household(db.Model):
     
     def __repr__(self):
         return f"Household('{self.id}', '{self.hType}')"
+        
+# class Member(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String(20), unique=False, nullable=False)
+    # gender = db.Column(db.String(20), unique=False, nullable=False)
+    # maritalStatus = db.Column(db.String(20), unique=False, nullable=False)
+    # spouse = db.Column(db.String(20), unique=False, nullable=False)
+    # occupationType = db.Column(db.String(20), unique=False, nullable=False)
+    # annualIncome = db.Column(db.String(20), unique=False, nullable=False)
+    # dob = db.Column(db.String(20), unique=False, nullable=False)
+    
+    # def __repr__(self):
+        # return f"Household('{self.id}', '{self.hType}')"
 
 
+# Routes
 @app.route("/")
 def home():
     #return "<h1>Hello</h1>"
@@ -54,6 +70,15 @@ def submit_household_create():
     last = str(db.session.query(Household).order_by(Household.id.desc()).first())
     createMessage = last + ' has been created. '
     return render_template('household_create.html', createMessage=createMessage);
+    
+@app.route("/addmember")
+def display_household_add_member():
+    return render_template('household_add_member.html');    
+    
+@app.route("/addmember",methods=["POST"])
+def submit_household_add_member():
+    return render_template('household_add_member.html');   
+
 
 if __name__ == "__main__":
     app.run()
