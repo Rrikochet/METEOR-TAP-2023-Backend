f39056a02e4e3c4af8a9d33addfbe7691011defb
Rrikochet <lionelyong.jh@gmail.com>
2022-09-22 18:56:09 +0800

    Conditions & Relationships for Household & Members

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask import Flask,redirect, url_for,render_template, url_for, flash, send_
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
+from datetime import datetime
 import secrets
 import string
 
@@ -19,24 +20,51 @@ db.create_all()
 
 # Classes
 class Household(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    hType = db.Column(db.String(20), unique=False, nullable=False)
+
+    __tablename__='households'
     
+    id = db.Column(db.Integer, primary_key=True)
+    hType = db.Column(db.String, unique=False, nullable=False)
+    
+    members = db.relationship('Member', backref='households', lazy=True)
+    
+    # Constructor
+    def __init__ (self, hType, notify=False):
+        self.hType = hType
+    
+    # "To String" Method
     def __repr__(self):
         return f"Household('{self.id}', '{self.hType}')"
         
-# class Member(db.Model):
-    # id = db.Column(db.Integer, primary_key=True)
-    # name = db.Column(db.String(20), unique=False, nullable=False)
-    # gender = db.Column(db.String(20), unique=False, nullable=False)
-    # maritalStatus = db.Column(db.String(20), unique=False, nullable=False)
-    # spouse = db.Column(db.String(20), unique=False, nullable=False)
-    # occupationType = db.Column(db.String(20), unique=False, nullable=False)
-    # annualIncome = db.Column(db.String(20), unique=False, nullable=False)
-    # dob = db.Column(db.String(20), unique=False, nullable=False)
+class Member(db.Model):
+
+    __tablename__='members'
     
-    # def __repr__(self):
-        # return f"Household('{self.id}', '{self.hType}')"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=False, nullable=False)
+    gender = db.Column(db.String, unique=False, nullable=False)
+    maritalStatus = db.Column(db.String, unique=False, nullable=False)
+    spouse = db.Column(db.Integer, unique=True, nullable=True)
+    occupationType = db.Column(db.String, unique=False, nullable=False)
+    annualIncome = db.Column(db.Float, unique=False, nullable=False)
+    dob = db.Column(db.Date, unique=False, nullable=False)
+    
+    household_id = db.Column(db.Integer, db.ForeignKey('households.id'), nullable=False)
+    
+    # Constructor
+    def __init__ (self, name, gender, maritalStatus, spouse, occupationType, annualIncome, dob, household_id, notify=False):
+        self.name = name
+        self.gender = gender
+        self.maritalStatus = maritalStatus
+        self.spouse = spouse
+        self.occupationType = occupationType
+        self.annualIncome = annualIncome
+        self.dob = dob
+        self.household_id = household_id
+    
+    # "To String" Method
+    def __repr__(self):
+        return f"Member('{self.id}', '{self.name}', '{self.gender}', '{self.maritalStatus}', '{self.spouse}', '{self.occupationType}', '{self.annualIncome}', '{self.dob}', '{self.household_id}')"
 
 
 # Routes
@@ -57,28 +85,75 @@ def pagenotfound():
 def admin():
     return redirect(url_for("pagenotfound"))
     
-@app.route("/create")
-def display_household_create():
-    return render_template('household_create.html');
-    
-@app.route("/create",methods=["POST"])
-def submit_household_create():
-    db.create_all()
-    hT = request.form["hType"]
-    db.session.add(Household(hType=hT))
-    db.session.commit()
-    last = str(db.session.query(Household).order_by(Household.id.desc()).first())
-    createMessage = last + ' has been created. '
-    return render_template('household_create.html', createMessage=createMessage);
-    
-@app.route("/addmember")
-def display_household_add_member():
-    return render_template('household_add_member.html');    
-    
-@app.route("/addmember",methods=["POST"])
-def submit_household_add_member():
-    return render_template('household_add_member.html');   
+@app.route("/create",methods=["GET", "POST"])
+def household_create():
+    # If Method is GET
+    if request.method == "GET":
+        return render_template('household_create.html');
+    # If Method is POST
+    elif request.method == "POST":
+        db.create_all()
+        hT = request.form["hType"]
+        db.session.add(Household(hType=hT))
+        db.session.commit()
+        last = str(db.session.query(Household.id).order_by(Household.id.desc()).limit(1).scalar())
+        createMessage = 'New " Household ' + last + ' " is created.'
+        return render_template('household_create.html', createMessage=createMessage);
+    # If Method is anything else
+    else: 
+        return redirect(url_for("pagenotfound"))
+   
+@app.route("/addmember",methods=["GET", "POST"])
+def household_add_member():
+    # If Method is GET
+    if request.method == "GET":
+        return render_template('household_add_member.html');
+    # If Method is POST
+    elif request.method == "POST":
+        try:
+            db.create_all()
+            n = request.form["name"]
+            g = request.form["gender"]
+            mS = request.form["maritalStatus"]
+            s = request.form["spouse"]
+            oT = request.form["occupationType"]
+            aI = request.form["annualIncome"]
+            d = request.form["dob"]
+            h = request.form["household_id"]
+            
+            # UNUSED to see all existing households
+            # existingHousehold = [id for id, in db.session.query(Household.id)]
+            
+            # Check if member is single, but inputs a spouse, immediately return message
+            if mS == "single" and s != "":
+                return render_template('household_add_member.html', addMemberMessage='You cannot be Single and have a Spouse');
+            
+            # Check if spouse already exists on some other member
+            if db.session.query(Member.id).filter_by(spouse=s).limit(1).scalar() is not None:
+                return render_template('household_add_member.html', addMemberMessage='Your Spouse is already taken');
+            
+            # If Member inputs a household value, use it, otherwise use latest household
+            if h == "":
+                h = float(db.session.query(Household.id).order_by(Household.id.desc()).limit(1).scalar())
+            
+            # Add Member
+            db.session.add(Member(name=n, gender=g, maritalStatus=mS, spouse=s, occupationType=oT, annualIncome=aI, dob=datetime.strptime(d, '%Y-%m-%d'), household_id=h))
+            db.session.commit()
+            
+            # If Add Member was successful, and If Member has spouse, make spouse have member's spouse id as well
+            if db.session.query(Member.id).filter_by(id=s).limit(1).scalar() == s:
+                db.session.query(Member.id).filter_by(id=s).update({spouse:db.session.query(Member.id).order_by(Member.id.desc()).limit(1).scalar()})
+                
+            
+            last = str(db.session.query(Member.id).order_by(Member.id.desc()).limit(1).scalar())
+            addMemberMessage = 'Member ' + last + ' has been created. '
+            return render_template('household_add_member.html', addMemberMessage=addMemberMessage);  
+        except Exception as e:
+            return render_template('household_add_member.html', addMemberMessage=e);
 
+    # If Method is anything else
+    else: 
+        return redirect(url_for("pagenotfound"))
 
 if __name__ == "__main__":
     app.run()
